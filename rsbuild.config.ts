44341dad768e348453e21fd1aad13236e3325aef
Ibrahim E.Gad <gad.i@turing.com>
2025-04-10 13:18:50 +0200

    Try to solve the 404 issue when accessing a specific route directly #2

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,7 +4,31 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig({
   html: {
     title: 'SN Coding Tools',
+    template: './public/index.html',
     favicon: './src/assets/turing.svg',
+    scriptLoading: 'defer',
+    inject: 'head',
+    meta: {
+      viewport: 'width=device-width, initial-scale=1.0',
+    },
+    templateParameters: {
+      title: 'SN Coding Tools',
+      ghPagesScript: `
+        // Single Page Apps for GitHub Pages
+        // MIT License
+        // https://github.com/rafgraph/spa-github-pages
+        (function(l) {
+          if (l.search[1] === '/' ) {
+            var decoded = l.search.slice(1).split('&').map(function(s) { 
+              return s.replace(/~and~/g, '&')
+            }).join('?');
+            window.history.replaceState(null, null,
+              l.pathname.slice(0, -1) + decoded + l.hash
+            );
+          }
+        }(window.location))
+      `,
+    },
   },
   plugins: [pluginReact()],
   server: {
